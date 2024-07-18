60b164f48acd516a043a6df0dd09132176c305c6
Lipughadei001 <ghadeilipu0@gmail.com>
2024-07-18 12:29:42 +0530

    Update data_alter.sql

--- data_alter.sql
+++ data_alter.sql
@@ -1,1 +1,9 @@
+-- MySQL dump 10.14  Distrib 5.5.68-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version       5.5.68-MariaDB
 
+USE `testdb6`;
+ALTER TABLE `exp1` ADD COLUMN `phone` varchar(20);
+UPDATE TABLE `exp1` SET `phone`='0000000000';
